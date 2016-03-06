88523fef14d8f038e0cfeb54674808679264e77b
赵雨函 <poligun@gmail.com>
2016-03-06 17:19:12 -0500

    Initial Commit

---
+++ Tagger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tagger
+//
+//  Created by ZhaoYuhan on 3/6/16.
+//  Copyright © 2016 ZhaoYuhan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
